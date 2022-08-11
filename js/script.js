d97b8726c5df45406de05efd8cccc8e3f584f048
Oleg Onoshko <onoshko.oleg@gmail.com>
2022-08-11 21:25:49 +0300

    fixed mistakes after feedback and added page aboutas

--- js/script.js
+++ js/script.js
@@ -2,5 +2,25 @@ $('#slider').slick({
     dots: true,
     infinite: true,
     slidesToShow: 1,
-    autoplay:true,
+    autoplay: true,
+    arrows: false,    
+});
+$('#sliderAbout').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    autoplay: true,
+    arrows: false, 
+    mobileFirst: true,
+    responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }   
+    }
+    ]
 });
